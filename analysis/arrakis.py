9cf1be092c4dc0885075e91a1ee99c2ef5aea6e4
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-07-21 09:07:45 -0700

    added generators

--- analysis/arrakis.py
+++ analysis/arrakis.py
@@ -5,3 +5,13 @@ import scipy as sp
 from matplotlib import pyplot as plt
 import uproot
 
+def print_details(input_file):
+    f = uproot.open(input_file)
+    for key in f.keys():
+        for item in f[key]:
+            print(f"{key}-{item}: {item.array(library='np')[0]}")
+
+
+
+if __name__ == "__main__":
+    print_details("larPyBridge_output.root")
