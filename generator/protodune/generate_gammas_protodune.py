0d72c6b985d19fbcefeb2d79fe0bd389ac5cd123
David Rivera <drivera@fnal.gov>
2023-07-03 17:10:04 -0500

    Updated the gamma generator to place gammas roughly at center of TPC2

--- generator/protodune/generate_gammas_protodune.py
+++ generator/protodune/generate_gammas_protodune.py
@@ -24,6 +24,11 @@ x_ddg = 355
 y_ddg = 630
 z_ddg = 60
 
+# Rough center location of TPC2
+x_center_TPC2 = 180.
+y_center_TPC2 = 303.
+z_center_TPC2 = 115.
+
 output_dir = "../../inputs/protodune/"
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
@@ -46,24 +51,24 @@ parser.add_argument(
     help='Mono-energetic gamma momentum in [GeV]. Default value is ~0.004745 GeV.'
 )
 parser.add_argument(
-    '--x_position', type=float, default=x_ddg,
-    help='x position of the gamma source in [cm]. Default value is 355 cm.'
+    '--x_position', type=float, default=x_center_TPC2,
+    help='x position of the gamma source in [cm]. Default value is 180 cm.'
 )
 parser.add_argument(
-    '--y_position', type=float, default=y_ddg,
-    help='y position of the gamma source in [cm]. Default value is 630 cm.'
+    '--y_position', type=float, default=y_center_TPC2,
+    help='y position of the gamma source in [cm]. Default value is 303 cm.'
 )
 parser.add_argument(
-    '--z_position', type=float, default=z_ddg,
-    help='z position of the gamma source in [cm]. Default value is 60 cm.'
+    '--z_position', type=float, default=z_center_TPC2,
+    help='z position of the gamma source in [cm]. Default value is 115 cm.'
 )
 parser.add_argument(
     '--output_dir', type=str, default='../../inputs/protodune/',
     help='Name of the output dir for this batch. Default value is "../../inputs/protodune/".'
 )
 parser.add_argument(
-    '--output_file', type=str, default='protodune_ddg',
-    help='Name of the output file for this batch. Default value is "protodune_ddg".'
+    '--output_file', type=str, default='protodune_ddg_gammas',
+    help='Name of the output file for this batch. Default value is "protodune_ddg_gammas".'
 )
 parser.add_argument(
     '--plot_momentum_distribution', type=bool, default=False,
